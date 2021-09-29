6d91a391173d1d9fca48066a82101acf561a092b
Давид Гобеджишвили <david.nodarovich@gmail.com>
2021-09-29 13:08:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x, y, a, b, c, S1, S2, S3, Sd;
+    cin >> a >> b >> c;
+    cin >> x >> y;
+    S1 = a * b;
+    S2 = c * b;
+    S3 = a * c;
+    Sd = x * y;
+    if (Sd >= S1 || Sd >= S2 || Sd >= S3) {
+        cout << "Vlezaet";
+    }
+    else {
+        cout << "Ne vlezaet";
+    }
+}
+
